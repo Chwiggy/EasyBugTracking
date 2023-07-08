985f430e7f28d506531deb869d30b8637dc0cfdd
Emily C. Wilke <emilycwilke@gmail.com>
2023-07-08 16:37:04 +0200

    compatibility changes

--- bug_tracking.py
+++ bug_tracking.py
@@ -1,20 +1,36 @@
+"""EmilyOS Bug tracking system"""
+
 class BugResolutionException(Exception):
     """Exception for bugs not resolvable by this software"""
     def __init__(self, name: str, description: str):
-        msg = self.format_message(name, description)
+        self.name = name
+        self.description = description
+        msg = self.format_message(name)
         super().__init__(msg)
 
     @staticmethod
-    def format_message(name, description):
+    def format_message(name: str):
+        """Formats the exception with a way to address it"""
         return f"Sorry we cannot help you with {name}, please consult the physical bug tracking."
 
-def bug_tracking():
-    print("Welcome to EmilyOS digital bug tracking")
-    bug_title = input("Give your bug a title: \n")
-    bug_description = input("Please describe your issue: \n")
-    raise BugResolutionException(bug_title, bug_description)
-    
+def receive_bug():
+    """Receive a bug from user input"""
+    title = input("Give your bug a title: \n")
+    description = input("Please describe your issue: \n")
+    return title, description
 
+def bug_tracking():
+    """Bug tracking main function"""
+    print("Welcome to EmilyOS digital bug tracking")
+    title, description = receive_bug()
+    raise BugResolutionException(title, description)
+
+def main():
+    """Run bug tracker and handle errors"""
+    try:
+        bug_tracking()
+    except BugResolutionException as exception:
+        print(exception)
 
 if __name__ == "__main__":
-    bug_tracking()
+    main()
